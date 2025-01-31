a723686d6a4ddb02a4c39e158d2500dce20232dd
Alexander Alemayhu <alexander@alemayhu.com>
2025-01-31 21:46:24 +0100

    add files

---
+++ src/main/java/socrates/ch/jokes/web/Config.java
@@ -0,0 +1,39 @@
+package socrates.ch.jokes.web;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.embedding.EmbeddingModel;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.ai.vectorstore.pgvector.PgVectorStore;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import static org.springframework.ai.vectorstore.pgvector.PgVectorStore.PgDistanceType.COSINE_DISTANCE;
+import static org.springframework.ai.vectorstore.pgvector.PgVectorStore.PgIndexType.HNSW;
+
+@Configuration
+class Config {
+
+    @Bean
+    ChatClient chatClient(ChatClient.Builder builder) {
+        return builder.defaultSystem("You are a expert dad joke teller. You will receive dad jokes in foreign languages and you will have to reply in that language. Do not reply with Sorry, I cannot answer the question as it is not in the context provided ")
+                .build();
+    }
+
+
+    @Primary
+    @Bean
+    public VectorStore vectorStore(JdbcTemplate jdbcTemplate, EmbeddingModel embeddingModel) {
+        return PgVectorStore.builder(jdbcTemplate, embeddingModel)
+                .dimensions(1536)                    // Optional: defaults to model dimensions or 1536
+                .distanceType(COSINE_DISTANCE)       // Optional: defaults to COSINE_DISTANCE
+                .indexType(HNSW)                     // Optional: defaults to HNSW
+                .initializeSchema(true)              // Optional: defaults to false
+                .schemaName("public")                // Optional: defaults to "public"
+                .vectorTableName("vector_store")     // Optional: defaults to "vector_store"
+                .maxDocumentBatchSize(10000)         // Optional: defaults to 10000
+                .build();
+    }
+
+}
--- src/main/java/socrates/ch/jokes/web/JokeController.java
+++ src/main/java/socrates/ch/jokes/web/JokeController.java
@@ -1,15 +1,55 @@
 package socrates.ch.jokes.web;
 
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.QuestionAnswerAdvisor;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.document.Document;
+import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import socrates.ch.jokes.model.Joke;
+
+import java.util.List;
 
 @RestController
 public class JokeController {
 
+    private final ChatClient chatClient;
+    private final VectorStore vectorStore;
+
+
+    public JokeController(ChatClient chatClient, VectorStore vectorStore) {
+        this.chatClient = chatClient;
+        this.vectorStore = vectorStore;
+
+
+        List<Document> documents = List.of(
+                new Document("Perché il libro di matematica è triste? Perché ha troppi problemi!"),  // Italian dad joke
+                new Document("Sai perché il pomodoro non si fa mai i fatti suoi? Perché è sempre troppo cotto!"),  // Italian dad joke
+                new Document("Cosa fa un cavallo quando è stanco? Si mette a riposo!"),  // Italian dad joke
+                new Document("Perché il computer è così bravo a fare amicizia? Perché è sempre in rete!"),  // Italian dad joke
+                new Document("Come si chiama un dinosauro con una grande testa? Un cervellone!"),  // Italian dad joke
+                new Document("Cosa fa un elettricista quando è arrabbiato? Si carica!"),  // Italian dad joke
+                new Document("Qual è il colmo per un pescatore? Avere le mani in acqua e non prendere mai niente!")  // Italian dad joke
+        );
+
+        vectorStore.add(documents);
+
+    }
+
     @GetMapping("/joke.html")
     public String getJoke() {
-        Joke joke = new Joke("Why don't scientists trust atoms? Because they make up everything!");
-        return "<p>" + joke.getContent() + "</p>";
+        String userInput = "Tell us a dad joke ";
+
+
+        return this.chatClient.prompt()
+                .user(userInput)
+                .advisors(
+                        new SimpleLoggerAdvisor(),
+                        new QuestionAnswerAdvisor(this.vectorStore))
+                .call()
+                .content();
+//        Joke joke = new Joke("Why don't scientists trust atoms? Because they make up everything!");
+//        return "<p>" + joke.getContent() + "</p>";
     }
 }
+
